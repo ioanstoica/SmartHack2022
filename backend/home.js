c9b4ae14e3567a05028b120d076288a840024393
ioanstoica <ioanstoica89@gmail.com>
2022-11-06 09:28:13 +0200

    cleaner

--- backend/home.js
+++ backend/home.js
@@ -1,18 +1,8 @@
 const express = require("express");
-const fs = require("fs");
-const ejs = require("ejs");
-const crypto = require("crypto");
-const path = require("path");
-const http = require("http");
-const formidable = require("formidable");
-const shell = require("shelljs");
 const {ethers} = require("hardhat");
-const ethers2 = require("ethers")
-const metamask = require("@metamask/detect-provider");
 const cookieParser = require("cookie-parser")
 const cors = require("cors") 
 const bodyParser = require("body-parser")
-
 
 app = express();
 app.set("view engine", "ejs");
